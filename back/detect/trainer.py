9abc9df180d6f955af5b92873dba1ceec45329fb
simonamador <samador0208@gmail.com>
2024-03-20 14:56:26 -0600

    updated backend framework + python req

--- back/detect/trainer.py
+++
@@ -1,5 +0,0 @@
-from ultralytics import YOLO
-
-model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
-
-# Training
---
+++ data_preproc.py
@@ -0,0 +1,43 @@
+import cv2
+import os
+import sys
+
+# Code by: @simonamador
+
+# Function to extract frames from a video until reaching the desired frame count
+def extract_frames(video_file, video_name):
+    cap = cv2.VideoCapture(video_file)
+    
+    frame_rate = 2  # Desired frame rate (1 frame every 0.5 seconds)
+    frame_count = 0
+    
+    # Get the video file's name without extension
+    # video_name = os.path.splitext(os.path.basename(video_file))[0]
+    
+    # Create an output folder with a name corresponding to the video
+    output_directory = "data/detector-tracker/"+f"{video_name}_frames"
+    os.makedirs(output_directory, exist_ok=True)
+    
+    while True:
+        ret, frame = cap.read()
+        
+        if not ret:
+            break
+        
+        frame_count += 1
+        
+        # Only extract frames at the desired frame rate
+        if frame_count % int(cap.get(5) / frame_rate) == 0:
+            new_frame = frame_count / int(cap.get(5) / frame_rate)
+            output_file = f"{output_directory}/frame_{new_frame}.jpg"
+            cv2.imwrite(output_file, frame)
+            print(f"Frame {frame_count} has been extracted and saved as {output_file}")
+    
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    name = sys.argv[1]
+    video_file = "data/detector-tracker/videos/"+name+".mp4"  # Replace with your video's name
+
+    extract_frames(video_file,name)
